4acfa7287467262c6c2fdc1d5d9e544a5379758d
Justin <justin.gray@reeldata.ai>
2024-01-31 21:55:23 -0400

    help docs

--- jgit.sh
+++ jgit.sh
@@ -92,6 +92,36 @@ clean () {
 }
 
 
+# Prints help and usage message
+help () {
+    echo """
+JGit - Justin's simple git repository and worktree manager
+
+Usage: $0 {repo|branch|clean} [args]
+
+    repo <name> <url>   Create a worktree repository
+    branch <name>       Create a new worktree for the given branch
+    clean               Delete local worktrees that do not have corresponding
+                        remote branches
+
+Typical usage:
+    1. Clone jgit repository to a directory
+        $ git clone https://github.com/just1ngray/jgit.git
+    2. Install jgit program to your system
+        $ cd jgit
+        $ sudo ./install.sh
+    3. Create a new worktree repository (perhaps in the same directory as the
+        jgit source)
+        $ jgit repo hello-world https://github.com/leachim6/hello-world.git
+    4. Create a new worktree for the given branch
+        $ cd hello-world
+        $ jgit branch main
+    5. Clean up old worktrees whose branch no longer exists on remote
+        $ jgit clean
+    """
+}
+
+
 case "$1" in
     repo)
         shift
@@ -104,9 +134,12 @@ case "$1" in
     clean)
         clean
         ;;
+    help|-h|--help)
+        help
+        ;;
     *)
-        echo "Usage: $0 {repo|branch|clean} [args]"
-        echo "Unknown command: '$1'"
+        help
+        echo -e "\nUnknown command: '$1'"
         exit 1
         ;;
 esac
