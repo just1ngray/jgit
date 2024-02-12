89b8d78682e1a0897e70dd8dfb3083118e7df7e1
Justin <justin.gray@reeldata.ai>
2024-02-11 23:28:36 -0400

    prompt? and create non-existing branches from branches on remote

--- jgit.sh
+++ jgit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Create a worktree repository
 repo () {
     local name=$1
@@ -26,28 +28,53 @@ repo () {
 branch () {
     local name=$1
 
+    # basic validation
     if [ -z "$name" ]; then
         echo "Usage: branch <name>"
         exit 1
     fi
-
     if [ ! -e .bare ]; then
         echo "This command must be executed from git repository"
         exit 1
     fi
-
     if [ -e "$name" ]; then
-        echo "Branch '$name' already exists"
+        echo "Branch '$name' already exists as a local worktree directory"
         exit 1
     fi
 
+    # sync with remote
     git fetch
+
+    # check if the selected branch exists; if not, then try creating it from another branch
+    if ! git ls-remote --exit-code origin "$name" > /dev/null; then
+        echo "Branch '$name' does not exist on remote"
+
+        local from_branch=$2
+        if [ -z "$from_branch" ]; then
+            default_branch=$(git symbolic-ref HEAD | sed "s/refs\/heads\///")
+            read -p "Create branch '$name' from which branch? ($default_branch): " from_branch
+            if [ -z "$from_branch" ]; then
+                from_branch=$default_branch
+            fi
+        fi
+
+        if ! git ls-remote --exit-code origin "$from_branch" > /dev/null; then
+            echo "Err: Branch '$from_branch' does not exist on remote"
+            exit 1
+        fi
+
+        echo "Creating branch '$name' from '$from_branch'"
+        git fetch origin "$from_branch":"$from_branch"
+        git push origin "$from_branch":"$name"
+    fi
+
+    # create the worktree and set it to track exactly the remote branch
     git worktree add "$name"
     cd "$name"
     git checkout "$name"
     git branch --set-upstream-to=origin/"$name"
-    git pull
     git reset --hard origin/"$name"
+    git pull
 }
 
 # Delete local worktrees that do not have corresponding remote branches
