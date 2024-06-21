aaf2527c05616fe1a20b334fb0ab24ec858692b2
Justin <justin.gray@reeldata.ai>
2024-06-21 17:18:19 -0300

    branch name autocomplete

--- bash_completion.sh
+++ bash_completion.sh
@@ -5,15 +5,19 @@ _jgit_completion() {
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="repo branch clean help -h --help"
+    prev2="${COMP_WORDS[COMP_CWORD-2]}"
 
-    case "${prev}" in
-        jgit)
-            COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-            return 0
-            ;;
-        *)
-            ;;
-    esac
+    if [[ "$prev" == "jgit" ]]; then
+        opts="repo branch clean help -h --help"
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    fi
+
+    if [[ "$prev" == "branch" || "$prev2" == "branch" ]]; then
+        opts=$(git branch -a | sed 's/remotes\/origin\///')
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    fi
 }
+
 complete -F _jgit_completion jgit
